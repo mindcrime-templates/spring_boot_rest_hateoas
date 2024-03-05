d9a5e5960c4a476466872367a00c71f4a6ac150f
Phillip Rhodes <motley.crue.fan@gmail.com>
2024-03-04 20:20:38 -0500

    Initial commit

---
+++ src/main/java/org/fogbeam/template/spring/spring_boot_rest/DemoApplication.java
@@ -0,0 +1,12 @@
+package org.fogbeam.template.spring.spring_boot_rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+}
